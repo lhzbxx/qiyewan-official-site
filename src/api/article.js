5b880a85dc55675b7657c6551febde1d2d270c29
yinJL <2041097701@qq.com>
2016-11-18 17:36:58 +0800

    更新文章API接口

--- src/api/article.js
+++ src/api/article.js
@@ -18,12 +18,12 @@ export default {
         Vue.http.get("articles/count?author=" + author).then( response => success(response), response => error(response));
     },
     getHomeNews(success, error){
-        Vue.http.get("http://192.168.1.7:3000/api/homeNews").then( response => success(response), response => error(response));
+        Vue.http.get("http://123.59.50.191:3001/api/homeNews").then( response => success(response), response => error(response));
     },
     getRecommendNews(success, error){
-        Vue.http.get("http://192.168.1.7:3000/api/recommendNews").then( response => success(response), response => error(response));
+        Vue.http.get("http://123.59.50.191:3001/api/recommendNews").then( response => success(response), response => error(response));
     },
     getArticle(articleId,success, error){
-        Vue.http.get("http://192.168.1.7:3000/api/articles/"+articleId).then( response => success(response), response => error(response));
+        Vue.http.get("http://123.59.50.191:3001/api/articles/"+articleId).then( response => success(response), response => error(response));
     }
 }
