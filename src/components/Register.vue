768b1a61149b5e8debb76aa259fddb55b407a508
lhzbxx <lhzbxx@gmail.com>
2016-11-23 22:17:46 +0800

    Fix.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -33,7 +33,7 @@
                 </el-form-item>
             </el-form>
             <el-button type="primary"
-                       @c-lick.native="submit"
+                       @click.native="submit"
                        :loading="isRegistering"
                        style="width: 100%">
                 {{ isRegistering ? "注册中" : "注 册" }}
