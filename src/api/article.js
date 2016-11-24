38fb1bfb33ca4a441e7dc8c7a3105f8876ab5d7e
yinJL <2041097701@qq.com>
2016-11-24 10:35:22 +0800

    投融资接口对接

--- src/api/article.js
+++ src/api/article.js
@@ -2,24 +2,27 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
+let invHost = 'http://123.59.50.191:3005';
+let articleHost = 'http://123.59.50.191:3001/api';
 
 export default {
+
     getHomeNews(success, error){
-        Vue.http.get("http://123.59.50.191:3001/api/homeNews").then(response => success(response), response => error(response));
+        Vue.http.get(articleHost+"/homeNews").then(response => success(response), response => error(response));
     },
 
     getRecommendNews(success, error){
-        Vue.http.get("http://123.59.50.191:3001/api/recommendNews").then(response => success(response), response => error(response));
+        Vue.http.get(articleHost+"/recommendNews").then(response => success(response), response => error(response));
     },
 
     getArticle(articleId, success, error){
-        Vue.http.get("http://123.59.50.191:3001/api/articles/" + articleId).then(response => success(response), response => error(response));
+        Vue.http.get(articleHost+"/articles/" + articleId).then(response => success(response), response => error(response));
     },
 
     getInvCaptcha(phone, success, error){
-        Vue.http.get("http://123.59.50.191:80/captcha/sms?mobile=" + phone).then(response => success(response), response => error(response));
+        Vue.http.get(invHost + "/captcha/sms?mobile=" + phone).then(response => success(response), response => error(response));
     },
-    getInvCaptcha(phone, success, error){
-        Vue.http.get("http://123.59.50.191:80/captcha/sms?mobile=" + phone).then(response => success(response), response => error(response));
+    postInvestments(formStacked, success, error){
+        Vue.http.post(invHost + "/investments", formStacked).then(response => success(response), response => error(response));
     }
 }
--- src/entry_dev.js
+++ src/entry_dev.js
@@ -1,6 +1,7 @@
 window.global_config = {
     mode: 'dev',
-    remote_url: 'http://127.0.0.1:8081',
+    remote_url: 'http://106.75.11.210:8081',
+    //remote_url: 'http://127.0.0.1:8081',
     router_mode: 'hash'
 }
 
