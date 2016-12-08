668f2fe0abfb5ff209c9e6b8041a3ad0e294f449
lhzbxx <lhzbxx@gmail.com>
2016-12-08 13:57:06 +0800

    Fix build config.

--- new/src/App.vue
+++ new/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <lh-hello></lh-hello>
   </div>
 </template>
 
@@ -11,18 +11,10 @@ import Hello from './components/Hello'
 export default {
   name: 'app',
   components: {
-    Hello
+    'lh-hello': Hello
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
