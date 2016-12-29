ce2598697fdc677a821b417b843a3c33a6aef2c8
lhzbxx <lhzbxx@gmail.com>
2016-12-29 18:34:52 +0800

    Add crm.js.

---
+++ src/api/crm.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
+Vue.http.options.root = process.env.API
+
+export default {
+  getCustomers (phone, cb, errorCb) {
+    Vue.http.get('customers?phone=' + phone).then((response) => {
+      cb(response.body)
+    }, (response) => {
+      errorCb(response.body)
+    })
+  },
+  getContracts (customerId, cb, errorCb) {
+    Vue.http.get('contracts?customerId=' + customerId).then((response) => {
+      cb(response.body)
+    }, (response) => {
+      errorCb(response.body)
+    })
+  },
+  getContractDetails (contractSno, cb, errorCb) {
+    Vue.http.get('contract-details?contractSno=' + contractSno).then((response) => {
+      cb(response.body)
+    }, (response) => {
+      errorCb(response.body)
+    })
+  },
+  getContractServices (contractSno, cb, errorCb) {
+    Vue.http.get('contract-services?contractSno=' + contractSno).then((response) => {
+      cb(response.body)
+    }, (response) => {
+      errorCb(response.body)
+    })
+  },
+  getContractServiceDetails (contractServiceId, cb, errorCb) {
+    Vue.http.get('contract-service-details?contractServiceId=' + contractServiceId).then((response) => {
+      cb(response.body)
+    }, (response) => {
+      errorCb(response.body)
+    })
+  }
+}
