94e7d82c7113068e2148098138d0cd13c38c1528
lhzbxx <lhzbxx@gmail.com>
2017-01-11 11:10:46 +0800

    Fix home page.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -21,10 +21,9 @@
           </div>
           <div class="slide-right"
                onmouseleave="this.style.transform='rotateX(0deg) rotateY(0deg)'"
-               onmousemove="x =
-               .clientY + document.body.scrollTop - 108;
-                          y = (event.clientX + document.body.scrollLeft - 360 - (window.screen.width - 1160) / 2);
-                          this.style.transform='rotateX(' + 15 * (x - 250) / 500 + 'deg) rotateY(' + 15 * (400 - y) / 800 + 'deg)'">
+               onmousemove="x = event.clientY + document.body.scrollTop - 108;
+               y = (event.clientX + document.body.scrollLeft - 360 - (window.screen.width - 1160) / 2);
+               this.style.transform='rotateX(' + 15 * (x - 250) / 500 + 'deg) rotateY(' + 15 * (400 - y) / 800 + 'deg)'">
             <div class="right-image" data-zindex="50" style="transform: translateZ(50px);">
               <img :src="item.imageBottomUrl" alt="">
             </div>
