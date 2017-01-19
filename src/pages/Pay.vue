b96d0e2fb6df6a9756dd26375091b8081c3ab7a7
lhzbxx <lhzbxx@gmail.com>
2017-01-19 17:15:00 +0800

    .

--- src/pages/Pay.vue
+++ src/pages/Pay.vue
@@ -1,110 +1,110 @@
 <template>
   <div id="order" class="container">
     <div v-if="getOrder">
-    <div style="border: 1px solid #eee;
+      <div style="border: 1px solid #eee;
                     border-bottom: none;
                     padding: 20px;">
-      <el-row style="height: 70px;">
-        <el-col :span="16"
-                style="height: 100%;
+        <el-row style="height: 70px;">
+          <el-col :span="16"
+                  style="height: 100%;
                                display: table;">
-          <div style="display: table-cell;
+            <div style="display: table-cell;
                                height: 100%;
                                vertical-align: middle;
                                font-size: 16px;">
-            <p>
-              <i class="el-icon-circle-check"
-                 style="font-size: 40px; color: #67d76c; margin-right: 10px; vertical-align: middle;"></i>
-              商品信息提交成功
-            </p>
-            </p>
-          </div>
-        </el-col>
-        <el-col :span="8"
-                style="text-align: right;
+              <p>
+                <i class="el-icon-circle-check"
+                   style="font-size: 40px; color: #67d76c; margin-right: 10px; vertical-align: middle;"></i>
+                商品信息提交成功
+              </p>
+              </p>
+            </div>
+          </el-col>
+          <el-col :span="8"
+                  style="text-align: right;
                                height: 100%;
                                display: table;">
-          <div style="display: table-cell; height: 100%; vertical-align: middle; font-size: 16px;">
-            应付金额
-            <span style="color: red; font-size: 24px;">{{ getOrder.totalPrice.toFixed(2) }}</span>元
-          </div>
-        </el-col>
-      </el-row>
-    </div>
-    <el-table
-      :data="getOrder.details"
-      selection-mode="multiple"
-      style="width: 100%">
-      <el-table-column
-        inline-template
-        label="商品信息"
-        width="420">
-        <div>
-          <el-row style="height: 90px;">
-            <el-col :span="5" style="height: 100%; display: table;">
-              <div style="width: 100%;
+            <div style="display: table-cell; height: 100%; vertical-align: middle; font-size: 16px;">
+              应付金额
+              <span style="color: red; font-size: 24px;">{{ getOrder.totalPrice.toFixed(2) }}</span>元
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+      <el-table
+        :data="getOrder.details"
+        selection-mode="multiple"
+        style="width: 100%">
+        <el-table-column
+          inline-template
+          label="商品信息"
+          width="420">
+          <div>
+            <el-row style="height: 90px;">
+              <el-col :span="5" style="height: 100%; display: table;">
+                <div style="width: 100%;
                                         height: 100%;
                                         display: table-cell;
                                         vertical-align: middle;">
-                <img :src="row.cover | cdn-filter"
-                     style="width: 100%;
+                  <img :src="row.cover | cdn-filter"
+                       style="width: 100%;
                                             display: table-cell;
                                             vertical-align: middle;">
-              </div>
-            </el-col>
-            <el-col :span="19" style="height: 100%; display: table;">
-              <div style="width: 100%;
+                </div>
+              </el-col>
+              <el-col :span="19" style="height: 100%; display: table;">
+                <div style="width: 100%;
                                         height: 100%;
                                         display: table-cell;
                                         vertical-align: middle;
                                         padding-left: 10px;">
-                <p style="line-height: 16px; margin-bottom: 5px;">
-                  {{ row.name }}
-                </p>
-                <p style="font-size: 13px; line-height: 15px; color: #aaa;">
-                  区域：{{ row.region }}
-                </p>
-              </div>
-            </el-col>
-          </el-row>
-        </div>
-      </el-table-column>
-      <el-table-column
-        inline-template
-        label="单价">
-        <div>&yen;
-          <span>{{ row.unitPrice }}</span>
-        </div>
-      </el-table-column>
-      <el-table-column
-        inline-template
-        label="数量">
-        <div>
-          <b>{{ row.amount }}</b>
-          <span>{{ row.unit }}</span>
-        </div>
-      </el-table-column>
-      <el-table-column
-        inline-template
-        width="300"
-        label="服务详情">
-        <div style="line-height: normal;">
-          <span>{{ row.summary }}</span>
-        </div>
-      </el-table-column>
-      <el-table-column
-        inline-template
-        property="totalPrice"
-        label="小计"
-        width="200">
-        <div style="color: red;">
-          &yen;
-          <span>{{ totalPrice(row) }}
+                  <p style="line-height: 16px; margin-bottom: 5px;">
+                    {{ row.name }}
+                  </p>
+                  <p style="font-size: 13px; line-height: 15px; color: #aaa;">
+                    区域：{{ row.region }}
+                  </p>
+                </div>
+              </el-col>
+            </el-row>
+          </div>
+        </el-table-column>
+        <el-table-column
+          inline-template
+          label="单价">
+          <div>&yen;
+            <span>{{ row.unitPrice }}</span>
+          </div>
+        </el-table-column>
+        <el-table-column
+          inline-template
+          label="数量">
+          <div>
+            <b>{{ row.amount }}</b>
+            <span>{{ row.unit }}</span>
+          </div>
+        </el-table-column>
+        <el-table-column
+          inline-template
+          width="300"
+          label="服务详情">
+          <div style="line-height: normal;">
+            <span>{{ row.summary }}</span>
+          </div>
+        </el-table-column>
+        <el-table-column
+          inline-template
+          property="totalPrice"
+          label="小计"
+          width="200">
+          <div style="color: red;">
+            &yen;
+            <span>{{ totalPrice(row) }}
             <span style="font-size: 13px;" v-show="row.premium > 0"> ( &plus; {{ row.premium.toFixed(2) }} ) </span>
           </span>
-        </div>
-      </el-table-column>
-    </el-table>
+          </div>
+        </el-table-column>
+      </el-table>
     </div>
     <br>
     <div>
@@ -167,6 +167,11 @@
       if (!this.getOrder) {
         this.$router.replace({name: 'order'})
       }
+      this.payments.forEach((value, index) => {
+        if (this.getOrder.payment === value.code) {
+          this.payment = index
+        }
+      })
     },
     methods: {
       addToOrder () {
